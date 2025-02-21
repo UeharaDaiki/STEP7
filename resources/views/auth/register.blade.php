a6812201f501f6091242dab8dd9e5c6e28278812
UeharaDaiki <u.daiki0122@gmail.com>
2025-02-21 22:26:12 +0900

    fix:ログイン後遷移先修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -50,7 +50,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('登録') }}
+                                    {{ __('register') }}
                                 </button>
                             </div>
                         </div>
