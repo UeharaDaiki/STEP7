883d05748e7877ef4b02708a79fbd153ab98de27
UeharaDaiki <u.daiki0122@gmail.com>
2025-03-06 22:22:09 +0900

    トランザクション追加

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -10,24 +10,34 @@ class SalesController extends Controller
 {
     //
     public function purchase(Request $request){
-        $productId = $request->input('product_id');
-        $quantity = $request->input('quantity',1);
+        try{
+            DB::beginTransaction();
+            $productId = $request->input('product_id');
+            $quantity = $request->input('quantity',1);
 
-        $product = Product::find($productId);
+            $product = Product::find($productId);
 
-        if(!$product){
-            return response()->json(['message' => '商品が存在しません'],404);
+            if(!$product){
+                return response()->json(['message' => '商品が存在しません'],404);
+            }
+            if($product->stock < $quantity){
+                return response()->json(['message' => '商品が在庫不足です'],400);
+            }
+            $product->stock-=$quantity;
+            $product->save();
+
+            $sale = new Sales;
+            $sale->product_id = $productId;
+            $sale->save();
+            
+            DB::commit();
+
+            return response()->json(['message' => '購入成功']);
+        }catch(\Exception $e){
+            DB::rollback();
+            \Log::error('Product registration failed: ' . $e->getMessage());
+            return back()->with('status', 'エラーが発生しました');
         }
-        if($product->stock < $quantity){
-            return response()->json(['message' => '商品が在庫不足です'],400);
-        }
-        $product->stock-=$quantity;
-        $product->save();
 
-        $sale = new Sales;
-        $sale->product_id = $productId;
-        $sale->save();
-
-        return response()->json(['message' => '購入成功']);
     }
 }
--- public/js/productDelete.js
+++ public/js/productDelete.js
@@ -1,102 +1,97 @@
-$(document).ready(function() {
-    $('.delete-btn').click(function(event) {
-        event.preventDefault();
-        var productId = $(this).data('id');
-        var row = $(this).closest('tr');
+$('.delete-btn').click(function(event) {
+    event.preventDefault();
+    var productId = $(this).data('id');
+    var row = $(this).closest('tr');
 
-        if (!confirm('本当に削除しますか？')) {
-            return;
-        }
+    if (!confirm('本当に削除しますか？')) {
+        return;
+    }
 
-        // CSRFトークンを取得
-        var csrfToken = $('meta[name="csrf-token"]').attr('content');
+    // CSRFトークンを取得
+    var csrfToken = $('meta[name="csrf-token"]').attr('content');
 
-        // AJAXリクエストを送信
-        $.ajax({
-            url: '/STEP7/public/product/delete/' + productId,
-            method: 'POST',
-            data: {
-                _token: csrfToken,
-            },
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            success: function(response) {
-                if (response.success) {
-                    row.fadeOut();
-                    console.log(response.redirect_url);
-                    window.location.href = response.redirect_url;
-                } else {
-                    alert('削除に失敗しました: ' + response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-                console.log('Request URL:', '/product/delete/' + productId);
-                console.log('AJAX Error:', error);
-                console.log('Status:', status);
-                console.log('Response Text:', xhr.responseText);
-                alert('削除中にエラーが発生しました: ' + error);
+    // AJAXリクエストを送信
+    $.ajax({
+        url: '/STEP7/public/product/delete/' + productId,
+        method: 'POST',
+        data: {
+            _token: csrfToken,
+        },
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        success: function(response) {
+            if (response.success) {
+                row.fadeOut();
+                console.log(response.redirect_url);
+                window.location.href = response.redirect_url;
+            } else {
+                alert('削除に失敗しました: ' + response.message);
             }
-        });
+        },
+        error: function(xhr, status, error) {
+            console.log('Request URL:', '/product/delete/' + productId);
+            console.log('AJAX Error:', error);
+            console.log('Status:', status);
+            console.log('Response Text:', xhr.responseText);
+            alert('削除中にエラーが発生しました: ' + error);
+        }
     });
 });
 
 
-$(document).ready(function() {
-    $('.search-btn').click(function() {
+$('.search-btn').click(function() {
 
-        if (!confirm('検索しますか？')) {
-            return;
-        }
-        var searchProductName = $('input[name="searchProductName"]').val();
-        var searchCompanyId = $('select[name="searchCompanyId"]').val();
-        var maxPrice = $('input[name="maxPrice"]').val();
-        var minPrice = $('input[name="minPrice"]').val();
-        var maxStock = $('input[name="maxStock"]').val();
-        var minStock = $('input[name="minStock"]').val();
+    if (!confirm('検索しますか？')) {
+        return;
+    }
+    var searchProductName = $('input[name="searchProductName"]').val();
+    var searchCompanyId = $('select[name="searchCompanyId"]').val();
+    var maxPrice = $('input[name="maxPrice"]').val();
+    var minPrice = $('input[name="minPrice"]').val();
+    var maxStock = $('input[name="maxStock"]').val();
+    var minStock = $('input[name="minStock"]').val();
 
-        // CSRFトークンを取得
-        var csrfToken = $('meta[name="csrf-token"]').attr('content');
+    // CSRFトークンを取得
+    var csrfToken = $('meta[name="csrf-token"]').attr('content');
 
-        // AJAXリクエストを送信
-        $.ajax({
-            url: '/STEP7/public/product/search',
-            method: 'POST',
-            data: {
-                _token: csrfToken,
-                searchProductName: searchProductName,
-                searchCompanyId: searchCompanyId,
-                maxPrice: maxPrice,
-                minPrice: minPrice,
-                maxStock: maxStock,
-                minStock: minStock
+    // AJAXリクエストを送信
+    $.ajax({
+        url: '/STEP7/public/product/search',
+        method: 'POST',
+        data: {
+            _token: csrfToken,
+            searchProductName: searchProductName,
+            searchCompanyId: searchCompanyId,
+            maxPrice: maxPrice,
+            minPrice: minPrice,
+            maxStock: maxStock,
+            minStock: minStock
+        },
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        success: function(response) {
+            console.log(maxPrice);
+            console.log(minPrice);
 
-            },
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            success: function(response) {
-                console.log(maxPrice);
-                console.log(minPrice);
-
-                if (response.success) {
-                    // 新しいページに遷移する前に、ページネーションを更新する
-                    $('.product-table tbody').html(response.productList);
-                    // ページネーションを更新
-                    $('.pagination').html(response.pagination);
-                    alert('検索できました');
-                } else {
-                    alert('検索に失敗しました: ' + response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-                console.log('Request URL:', '/product/search');
-                console.log('AJAX Error:', error);
-                console.log('Status:', status);
-                console.log('Response Text:', xhr.responseText);
-                alert('検索中にエラーが発生しました: ' + error);
+            if (response.success) {
+                // 新しいページに遷移する前に、ページネーションを更新する
+                $('.product-table tbody').html(response.productList);
+                // ページネーションを更新
+                $('.pagination').html(response.pagination);
+                alert('検索できました');
+            } else {
+                alert('検索に失敗しました: ' + response.message);
             }
-        });
+        },
+        error: function(xhr, status, error) {
+            console.log('Request URL:', '/product/search');
+            console.log('AJAX Error:', error);
+            console.log('Status:', status);
+            console.log('Response Text:', xhr.responseText);
+            alert('検索中にエラーが発生しました: ' + error);
+        }
     });
 });
 
